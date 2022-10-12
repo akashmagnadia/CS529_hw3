dfdf99c827e31643172917f1dac32366c9863863
Akash <akash.magnadia@gmail.com>
2022-10-12 13:25:15 -0500

    Initial commit

---
+++ src/js/particleSystem.js
@@ -0,0 +1,70 @@
+var data = [];
+
+// bounds of the data
+const bounds = {};
+
+// create the containment box.
+// This cylinder is only to guide development.
+// TODO: Remove after the data has been rendered
+const createCylinder = () => {
+    // get the radius and height based on the data bounds
+    const radius = (bounds.maxX - bounds.minX) / 2.0 + 1;
+    const height = (bounds.maxY - bounds.minY) + 1;
+
+    // create a cylinder to contain the particle system
+    const geometry = new THREE.CylinderGeometry(radius, radius, height, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
+    const cylinder = new THREE.Mesh(geometry, material);
+
+    // add the containment to the scene
+    scene.add(cylinder);
+};
+
+// creates the particle system
+const createParticleSystem = (data) => {
+    // draw your particle system here!
+};
+
+const loadData = (file) => {
+
+    // read the csv file
+    d3.csv(file).then(function (fileData)
+    // iterate over the rows of the csv file
+    {
+        fileData.forEach(d => {
+            // get the min bounds
+            bounds.minX = Math.min(bounds.minX || Infinity, d.Points0);
+            bounds.minY = Math.min(bounds.minY || Infinity, d.Points1);
+            bounds.minZ = Math.min(bounds.minZ || Infinity, d.Points2);
+
+            // get the max bounds
+            bounds.maxX = Math.max(bounds.maxX || -Infinity, d.Points0);
+            bounds.maxY = Math.max(bounds.maxY || -Infinity, d.Points1);
+            bounds.maxZ = Math.max(bounds.maxY || -Infinity, d.Points2);
+
+            // add the element to the data collection
+            data.push({
+                // concentration density
+                concentration: Number(d.concentration),
+                // Position
+                X: Number(d.Points0),
+                Y: Number(d.Points1),
+                Z: Number(d.Points2),
+                // Velocity
+                U: Number(d.velocity0),
+                V: Number(d.velocity1),
+                W: Number(d.velocity2)
+            })
+        });
+        // draw the containment cylinder
+        // TODO: Remove after the data has been rendered
+        createCylinder()
+        // create the particle system
+        // createParticleSystem(data);
+    })
+
+
+};
+
+
+loadData('data/058.csv');
